4944141ea24df6255669ec127f8f4c27691d8d49
Gustavo Cateim <cateim@gmail.com>
2025-10-25 03:49:57 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+# /srv/camilla/update.sh
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+
+echo "--- Verificando atualizações em $(date) ---"
+
+git fetch origin main
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin/main)
+
+if [ "$LOCAL" != "$REMOTE" ]; then
+  echo "Atualização detectada. Executando git pull..."
+  git reset --hard origin/main
+  git clean -fd
+  docker exec camilla nginx -s reload
+  echo "Atualização aplicada em $(date)"
+else
+  echo "Nenhuma atualização encontrada."
+fi
+
+echo "Processo finalizado."
